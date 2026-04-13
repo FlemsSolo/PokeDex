df8ab7b823cd6cb419f32092d56a26bcf63315c3
FlemsSolo <354019@via.dk>
2026-04-13 14:09:16 +0200

    2 commits

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createHashRouter, RouterProvider } from "react-router-dom";
 import './index.css'
+
 import App from './App.jsx'
 
 createRoot(document.getElementById('MyRoot')).render(
