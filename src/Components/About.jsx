7c5920b2310e56310d8830933c8ca40bc209461b
FlemsSolo <354019@via.dk>
2026-04-18 13:19:58 +0200

    Single Period Commit

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -5,7 +5,7 @@ export function About()
     <div style={{aborder: '1px solid #808080', padding: '12px' }}>
     <h2>Info About PokeDex</h2>
     <img src='./public/WillieAndPika.png' style={{border: '1px solid #808080', maxHeight: '70vh' }} />
-    <h3>Willie And Pika Discussing Layout ...</h3>
+    <h3>Willie And Pika Discussing Layout ....</h3>
     </div>
   );
   }
