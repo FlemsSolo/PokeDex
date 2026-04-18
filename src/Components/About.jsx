cd9fb38332fd4e794ce0f509a98af8dc08d823ab
FlemsSolo <354019@via.dk>
2026-04-18 14:38:27 +0200

    Images to Images ...

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -4,7 +4,7 @@ export function About()
   return (
     <div style={{aborder: '1px solid #808080', padding: '12px' }}>
     <h2>Info About PokeDex</h2>
-    <img src='./public/WillieAndPika.png' style={{border: '1px solid #808080', maxHeight: '70vh' }} />
+    <img src='./images/WillieAndPika.png' style={{border: '1px solid #808080', maxHeight: '70vh' }} />
     <h3>Willie And Pika Discussing Layout ....</h3>
     </div>
   );
